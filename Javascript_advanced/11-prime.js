75ba9ed01dd03c0e6a6651aae8e57fc928e5d96f
AriRainbow <mariel.williams@atlasschool.com>
2025-07-31 13:04:56 -0500

    x

--- Javascript_advanced/11-prime.js
+++ Javascript_advanced/11-prime.js
@@ -20,16 +20,17 @@ function countPrimeNumbers() {
   }
   
   // Defer execution using setTimeout (pushes to end of event loop)
+  const t0 = performance.now();
   setTimeout(() => {
     const t0 = performance.now();
   
     for (let i = 0; i < 100; i++) {
       countPrimeNumbers();
     }
-  
+  }, 0);
     const t1 = performance.now();
   
     console.log(
       `Execution time of calculating prime numbers 100 times was ${t1 - t0} milliseconds.`
     );
-  }, 0);
+ 
